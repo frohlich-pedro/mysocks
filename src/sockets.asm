1bf15730a0faf0ef3723f9214b648dfee0e2a652
Pedro Fröhlich <pedro_h_nf@outlook.com>
2025-07-19 14:46:12 +0000

    nasm syntax is way clearer, so i changed it :)

---
+++ src/sockets.asm
@@ -0,0 +1,57 @@
+bits 64
+
+global socket
+global bind
+global listen
+global accept
+global setopt
+global close
+
+section .text
+
+socket:
+	mov rax, 97
+	mov rdi, rdi
+	mov rsi, rsi
+	mov rdx, rdx
+	syscall
+	ret
+
+bind:
+	mov rax, 104
+	mov rdi, rdi
+	mov rsi, rsi
+	mov rdx, rdx
+	syscall
+	ret
+
+listen:
+	mov rax, 106
+	mov rdi, rdi
+	mov rsi, rsi
+	syscall
+	ret
+
+accept:
+	mov rax, 30
+	mov rdi, rdi
+	mov rsi, rsi
+	mov rdx, rdx
+	syscall
+	ret
+
+setopt:
+	mov rax, 105
+	mov rdi, rdi
+	mov rsi, rsi
+	mov rdx, rdx
+	mov rcx, rcx
+	mov r8, r8
+	syscall
+	ret
+
+close:
+	mov rax, 6
+	mov rdi, rdi
+	syscall
+	ret
